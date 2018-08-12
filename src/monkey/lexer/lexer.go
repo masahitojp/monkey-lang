6822467f687955e705f311603c978b8add6840b0
masahitojp <randomstep@gmail.com>
2018-08-12 23:58:32 +0900

    feat: change lexer_test

--- src/monkey/lexer/lexer.go
+++ src/monkey/lexer/lexer.go
@@ -25,6 +25,18 @@ func (l *Lexer) readChar() {
 	l.readPosition += 1
 }
 
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.readPosition]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
@@ -48,6 +60,13 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 
 	l.readChar()
