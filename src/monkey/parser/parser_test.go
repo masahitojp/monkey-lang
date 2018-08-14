27bfb2b6c8ace24a65fbb78e7251cd43e2822b51
masahitojp <randomstep@gmail.com>
2018-08-15 00:03:43 +0900

    feat: :sparkles: sec 2.8.2

--- src/monkey/parser/parser_test.go
+++ src/monkey/parser/parser_test.go
@@ -311,6 +311,18 @@ func TestOperatorPrecedenceParsing(t *testing.T) {
 			"3 < 5 == true",
 			"((3 < 5) == true)",
 		},
+		{
+			"1 + (2 + 3) + 4",
+			"((1 + (2 + 3)) + 4)",
+		},
+		{
+			"(5 + 5) * 2",
+			"((5 + 5) * 2)",
+		},
+		{
+			"2 / (5 + 5)",
+			"(2 / (5 + 5))",
+		},
 	}
 
 	for _, tt := range tests {
